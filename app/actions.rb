1ae619996f11503cb7e2251e1d5fae6d9649f144
Will Beange <will@convertstar.com>
2015-08-30 16:34:49 -0700

    users - initial commit - attempting to code

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,20 @@
 get '/' do
   erb :index
 end
+
+get '/users' do
+  @users = User.all
+  erb :users
+end
+
+# post '/users' do
+
+# end
+
+# put '/users/:user_id' do
+
+# end
+
+# delete '/users/:user_id' do
+
+# end
